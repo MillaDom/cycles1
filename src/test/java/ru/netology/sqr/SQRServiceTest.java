37aa6813f9c4d1e2f695670bc968121dc828a21a
MillaDom <milladom@gmail.com>
2022-12-20 16:48:33 +0200

    reformatted code

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -7,45 +7,45 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @Test
-    public void CalculateSquaresInInterval(){
+    public void CalculateSquaresInInterval() {
         SQRService service = new SQRService();
-        int actual = service.calcSquares(200,300);
+        int actual = service.calcSquares(200, 300);
         int expected = 3;
         assertEquals(expected, actual);
 
     }
 
     @Test
-    public void CalculateSquaresOutIntervalPlusMin(){
+    public void CalculateSquaresOutIntervalPlusMin() {
         SQRService service = new SQRService();
-        int actual = service.calcSquares(0,100);
+        int actual = service.calcSquares(0, 100);
         int expected = 1;
         assertEquals(expected, actual);
 
     }
 
     @Test
-    public void CalculateSquaresOutIntervalPlusMax(){
+    public void CalculateSquaresOutIntervalPlusMax() {
         SQRService service = new SQRService();
-        int actual = service.calcSquares(9801,100000);
+        int actual = service.calcSquares(9801, 100000);
         int expected = 1;
         assertEquals(expected, actual);
 
     }
 
     @Test
-    public void CalculateSquaresOutIntervalMin(){
+    public void CalculateSquaresOutIntervalMin() {
         SQRService service = new SQRService();
-        int actual = service.calcSquares(0,99);
+        int actual = service.calcSquares(0, 99);
         int expected = 0;
         assertEquals(expected, actual);
 
     }
 
     @Test
-    public void CalculateSquaresOutIntervalMax(){
+    public void CalculateSquaresOutIntervalMax() {
         SQRService service = new SQRService();
-        int actual = service.calcSquares(9802,99999);
+        int actual = service.calcSquares(9802, 99999);
         int expected = 0;
         assertEquals(expected, actual);
 
